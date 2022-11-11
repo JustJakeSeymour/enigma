20cf6a9c4e75a11776aa6fbaeca83e70cd7f5f75
J Seymour <JustJakeSeymour@gmail.com>
2022-11-11 15:20:12 -0700

    write happy path tests for shift, and build to spec

--- lib/key.rb
+++ lib/key.rb
@@ -13,10 +13,10 @@ class Key
 
   def hash
     {
-      :A => "#{@key[0]}#{@key[1]}",
-      :B => "#{@key[1]}#{@key[2]}",
-      :C => "#{@key[2]}#{@key[3]}",
-      :D => "#{@key[3]}#{@key[4]}"
+      :A => "#{@key[0]}#{@key[1]}".to_i,
+      :B => "#{@key[1]}#{@key[2]}".to_i,
+      :C => "#{@key[2]}#{@key[3]}".to_i,
+      :D => "#{@key[3]}#{@key[4]}".to_i
     }
   end
 end
