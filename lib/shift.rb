1b68348cca817ff02aadc74482ffb4aff1a84af2
J Seymour <JustJakeSeymour@gmail.com>
2022-11-12 15:27:26 -0700

    move alphabet related methods to shift class

--- lib/shift.rb
+++ lib/shift.rb
@@ -8,4 +8,23 @@ class Shift
   def combination(key, offset)
     key.merge(offset){|symbol, k, o| k + o}
   end
+
+  # not directly tested
+  def alphabet_array
+    ('a'..'z').to_a.push(" ")
+  end
+
+  def rotate_forwards
+    [alphabet_array.rotate(@hash[:A]),
+     alphabet_array.rotate(@hash[:B]),
+     alphabet_array.rotate(@hash[:C]),
+     alphabet_array.rotate(@hash[:D])]
+  end
+
+  def rotate_backwards
+    [alphabet_array.rotate(-@hash[:A]),
+     alphabet_array.rotate(-@hash[:B]),
+     alphabet_array.rotate(-@hash[:C]),
+     alphabet_array.rotate(-@hash[:D])]
+  end
 end
